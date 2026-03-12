e65146e90f5808147c4308d591fd912c6e447211
kamrank35 <119065949+kamrank35@users.noreply.github.com>
2026-03-12 12:44:10 +0530

    fix navbar routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-// import React,{ useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
-import AddItem from './pages/Additem';
-import EditItem from './pages/Edititem';
-import ItemList from './pages/Itemlist';
-import { Routes,Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
+import AddItem from './pages/Additem'
+import EditItem from './pages/Edititem'
+import ItemList from './pages/Itemlist'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
+  return (
+    <>
+      <Navbar />
 
-  return (<>
-    <Navbar />
-    <Sidebar />
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/add-item" element={<AddItem />} />
-      <Route path="/items" element={<ItemList />} />
-      <Route path="/edit/:id" element={<EditItem />} />
-      <Route path="*" element={<h1>Page Not Found</h1>} />
-    </Routes>
+      <div className="container-fluid">
+        <div className="row">
+
+          <Sidebar />
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 mt-4">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/add-item" element={<AddItem />} />
+              <Route path="/items" element={<ItemList />} />
+              <Route path="/edit/:id" element={<EditItem />} />
+              <Route path="*" element={<h1>Page Not Found</h1>} />
+            </Routes>
+          </main>
+
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,14 @@ function Navbar() {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="navbar-collapse" id="navbarSupportedContent">
+        <div className=" navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
             <li className="nav-item">
