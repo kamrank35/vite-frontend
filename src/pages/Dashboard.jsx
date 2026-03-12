c8f9f1082abc94c8cd15905b1204628cc0aa3dc5
kamrank35 <119065949+kamrank35@users.noreply.github.com>
2026-03-12 16:07:38 +0530

    use state adding

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 function Dashboard() {
-    return <h1>Dashboard Page</h1>;
+    const [counter, setCounter] = useState(0);
+    
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            <h2>{counter}</h2>
+            <button className="btn btn-dark" onClick={() => setCounter(counter+1)}>Increase</button>
+            <button className="btn btn-dark" onClick={() => setCounter(counter-1)}>Decrease</button>
+        </div>
+    )
 }
 
 export default Dashboard;
