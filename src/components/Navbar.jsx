cc7375e11cb30b1100c658f62c6a3cd52e8874e9
kamrank35 <119065949+kamrank35@users.noreply.github.com>
2026-03-12 11:47:42 +0530

    fix navbar routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className=" navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
             <li className="nav-item">
@@ -32,16 +32,7 @@ function Navbar() {
 
           </ul>
 
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-            />
-            <button className="btn btn-outline-success">
-              Search
-            </button>
-          </form>
+          
         </div>
       </div>
     </nav>
